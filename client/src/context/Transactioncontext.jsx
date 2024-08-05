b8ff1686ce98d0794e0c099d8a5d583ce8e25a5d
Jagath K R <108512623+jagath411@users.noreply.github.com>
2024-08-05 22:12:22 +0530

    Service Update

--- client/src/context/Transactioncontext.jsx
+++ client/src/context/Transactioncontext.jsx
@@ -6,6 +6,7 @@ import { contractABI, contractAddress } from "../utils/constants";
 export const TransactionContext = React.createContext();
 
 const { ethereum } = window;
+
 const createEthereumContract = () => {
   const provider = new ethers.providers.Web3Provider(ethereum);
   const signer = provider.getSigner();
